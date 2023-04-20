6a19ec9c84827b63a7b2945d2eb9cd0d483a61bf
codewithamans <a.jagat159753@gmail.com>
2023-04-21 01:27:02 +0530

    props passed successfully

---
+++ src/components/Course_Card.vue
@@ -0,0 +1,31 @@
+<template>
+  <div :class="bgClass">
+    <div
+      class="text-[#01CB63] absolute bottom-5 right-5 w-[125px] italic flex justify-center items-center bg-[#000] h-[40px] rounded-lg"
+    >
+      Business
+    </div>
+  </div>
+  <div class="w-4/5 mt-5 font-bold tracking-wider text-xl mx-4">
+    Ultimate Business Intelligence Analyst A to Z Course 2021
+  </div>
+  <div class="w-4/5 mt-5 text-sm mx-4">
+    <span class="font-bold">46995</span> <span class="italic"> Student</span>
+  </div>
+  <div class="w-full flex justify-between">
+    <div class="ml-3">1</div>
+    <div class="mr-3">2</div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  topa: String,
+  bgClass: {
+    type: String,
+    default: "bg-yellow-300",
+  },
+});
+</script>
--- src/components/Courses.vue
+++ src/components/Courses.vue
@@ -1,10 +1,30 @@
 <template>
-  <div class="w-full bg-yellow h-auto bg-yellow-400">
+  <div class="w-full bg-yellow h-auto">
     <div class="w-full text-[#01CB63] italic text-center mt-32 text-2xl">
       Courses
     </div>
     <div class="w-full text-black text-center mt-4 text-4xl">
       Our Most Popular Courses
     </div>
+    <div class="flex justify-center items-center">
+      <div class="w-4/5 bg-red-300">
+        <div class="grid lg:grid-cols-3 gap-24">
+          <div class="bg-yellow-300 h-[500px]">
+            <Course_Card
+              topa="topa"
+              bgClass="bg-girl w-full h-[300px] relative bg-slate-500"
+            />
+          </div>
+          <div class="bg-yellow-300">2</div>
+          <div>3</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+import Course_Card from "./Course_Card.vue";
+export default {
+  components: { Course_Card },
+};
+</script>
