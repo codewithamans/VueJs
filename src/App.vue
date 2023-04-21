d8d3f4559c05b83faff5014a1dc4c7101ef426bc
codewithamans <a.jagat159753@gmail.com>
2023-04-21 18:28:48 +0530

    earner part ready

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Courses from "./components/Courses.vue";
+import Earners from "./components/Earners.vue";
 import Main from "./components/Main.vue";
 import Navbar from "./components/Navbar.vue";
 </script>
@@ -8,6 +9,7 @@ import Navbar from "./components/Navbar.vue";
   <Navbar />
   <Main />
   <Courses />
+  <Earners />
 </template>
 
 <style scoped>
---
+++ src/components/Earners.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="w-full flex justify-center">
+    <div class="w-4/5 mt-36">
+      <div class="italic text-[#01CB63] font-semibold text-xl">Top Earners</div>
+      <div class="text-black mt-4 text-4xl font-bold">
+        Transforming Your Ideas
+      </div>
+      <div class="text-black mt-2 text-4xl font-bold">Into Reality</div>
+      <div class="grid lg:grid-cols-4 gap-8 mt-20">
+        <div>
+          <div>
+            <img src="@/assets/images/luis.png" alt="" srcset="" />
+          </div>
+          <div class="flex space-x-4 mt-4 items-center">
+            <div class="bg-[#01CB63] w-[10px] h-[50px]"></div>
+            <div class="font-bold text-xl">&#x20B9; 8,00,000</div>
+          </div>
+        </div>
+        <div>
+          <div>
+            <img src="@/assets/images/craig.png" alt="" srcset="" />
+          </div>
+          <div class="flex space-x-4 mt-4 items-center">
+            <div class="bg-[#01CB63] w-[10px] h-[50px]"></div>
+            <div class="font-bold text-xl">&#x20B9; 6,00,000</div>
+          </div>
+        </div>
+        <div>
+          <div>
+            <img src="@/assets/images/chris.png" alt="" srcset="" />
+          </div>
+          <div class="flex space-x-4 mt-4 items-center">
+            <div class="bg-[#01CB63] w-[10px] h-[50px]"></div>
+            <div class="font-bold text-xl">&#x20B9; 5,00,000</div>
+          </div>
+        </div>
+        <div>
+          <div>
+            <img src="@/assets/images/jonas.png" alt="" srcset="" />
+          </div>
+          <div class="flex space-x-4 mt-4 items-center">
+            <div class="bg-[#01CB63] w-[10px] h-[50px]"></div>
+            <div class="font-bold text-xl">&#x20B9; 4,50,000</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
