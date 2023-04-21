bf1ee3c9ee5cf2dcafbbd2d1f2744270ea3b001f
codewithamans <a.jagat159753@gmail.com>
2023-04-21 18:07:18 +0530

    courses ready

--- src/components/Course_Card.vue
+++ src/components/Course_Card.vue
@@ -3,18 +3,49 @@
     <div
       class="text-[#01CB63] absolute bottom-5 right-5 w-[125px] italic flex justify-center items-center bg-[#000] h-[40px] rounded-lg"
     >
-      Business
+      {{ title }}
     </div>
   </div>
   <div class="w-4/5 mt-5 font-bold tracking-wider text-xl mx-4">
-    Ultimate Business Intelligence Analyst A to Z Course 2021
+    {{ desc }}
   </div>
   <div class="w-4/5 mt-5 text-sm mx-4">
-    <span class="font-bold">46995</span> <span class="italic"> Student</span>
+    <span class="font-bold">{{ student_no }}</span>
+    <span class="italic"> Student</span>
   </div>
-  <div class="w-full flex justify-between">
-    <div class="ml-3">1</div>
-    <div class="mr-3">2</div>
+  <div class="w-full flex justify-between mt-5">
+    <div class="ml-3 flex space-x-2">
+      <div><img src="@/assets/images/schedule.svg" alt="" srcset="" /></div>
+      <div class="italic text-[#86919D] font-semibold">{{ time }}</div>
+    </div>
+    <div class="mr-3 flex space-x-2">
+      <div><img src="@/assets/images/time.svg" alt="" srcset="" /></div>
+      <div class="italic text-[#86919D] font-semibold">
+        {{ lectures }} Lectures
+      </div>
+    </div>
+  </div>
+  <hr class="w-[25rem] mt-5 ml-5 bg-[#86919D]" />
+  <div class="w-full flex justify-between mt-5 mb-3">
+    <div class="ml-3 flex space-x-2">
+      <div>
+        <img
+          src="@/assets/images/woodwatch.jpg"
+          class="rounded-full w-[3rem] h-[3rem]"
+          alt="No image"
+          srcset=""
+        />
+      </div>
+      <div class="text-black font-bold mt-3 ml-3">{{ name }}</div>
+    </div>
+    <div class="mr-3 flex space-x-2">
+      <div class="text-[#01CB63] font-bold mt-2 text-xl">
+        &#x20B9; {{ price }}
+      </div>
+      <div class="text-[#86919D] text-sm mt-[14px] line-through">
+        {{ price }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -22,7 +53,13 @@
 import { defineProps } from "vue";
 
 const props = defineProps({
-  topa: String,
+  title: String,
+  desc: String,
+  student_no: String,
+  time: String,
+  lectures: String,
+  name: String,
+  price: String,
   bgClass: {
     type: String,
     default: "bg-yellow-300",
--- src/components/Courses.vue
+++ src/components/Courses.vue
@@ -7,16 +7,45 @@
       Our Most Popular Courses
     </div>
     <div class="flex justify-center items-center">
-      <div class="w-4/5 bg-red-300">
-        <div class="grid lg:grid-cols-3 gap-24">
-          <div class="bg-yellow-300 h-[500px]">
+      <div class="w-4/5">
+        <div class="grid lg:grid-cols-3 mt-20 gap-24">
+          <div class="h-auto shadow-lg shadow-gray-400 rounded-xl">
             <Course_Card
-              topa="topa"
-              bgClass="bg-girl w-full h-[300px] relative bg-slate-500"
+              title="Business"
+              desc=" Ultimate Business Intelligence Analyst A to Z Course 2021"
+              student_no="46,995"
+              time="11 hr 20 mins"
+              lectures="44"
+              img="@/assets/images/carla.jpg"
+              name="Albert Hitler"
+              price="2000"
+              bgClass="bg-business rounded-xl w-full h-[300px] relative bg-slate-500"
             />
           </div>
-          <div class="bg-yellow-300">2</div>
-          <div>3</div>
+          <div class="h-auto shadow-lg shadow-gray-400 rounded-xl">
+            <Course_Card
+              title="Design"
+              desc="Adobe Photoshop Training: From Beginner to Pro"
+              student_no="6,995"
+              time="6 hr 20 mins"
+              lectures="24"
+              name="Wanda Wilson"
+              price="3000"
+              bgClass="bg-design rounded-xl w-full h-[300px] relative bg-slate-500"
+            />
+          </div>
+          <div class="h-auto shadow-lg shadow-gray-400 rounded-xl">
+            <Course_Card
+              title="Marketing"
+              desc="Social Media Marketing - A to Z Content Marketing class 2021"
+              student_no="46,995"
+              time="11 hr 20 mins"
+              lectures="44"
+              name="Albert Hitler"
+              price="2000"
+              bgClass="bg-marketing rounded-xl w-full h-[300px] relative bg-slate-500"
+            />
+          </div>
         </div>
       </div>
     </div>
